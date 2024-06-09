a313201eb7e504c62a59d714f477540f24358812
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 17:36:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+let magiciansName = ["Ali", "Mustufa", "Haider", "Sohil", "Hammad"];
+function show_magicians() {
+    for (let a of magiciansName) {
+        console.log(magiciansName);
+    }
+}
+show_magicians();
---
+++ main.ts
@@ -0,0 +1,7 @@
+   let magiciansName:string []  =["Ali","Mustufa","Haider","Sohil","Hammad"]
+   function show_magicians() {
+    for (let a of magiciansName) {
+        console.log(magiciansName);
+    }
+   }
+   show_magicians();
